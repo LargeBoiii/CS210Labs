6b22d444998c634a5f4eec43dff1d4da566b37e1
LargeBoiii <mikolaj.majka04@gmail.com>
2024-11-29 17:30:46 +0000

    Lab 7 completed, doesnt really work too well tho :(

--- CS210LabSeven/CS210LabSevenMain.java
+++ CS210LabSeven/CS210LabSevenMain.java
@@ -8,35 +8,98 @@ public class CS210LabSevenMain
     {
         LinkedList list = new LinkedList();
         Scanner sc = new Scanner(System.in);
-        list.insertAtHead("joe",24,"comp sci",5);
-        list.insertAtHead("jane",50,"nursing",4);
-        list.insertAtHead("bob",99,"kush growing",3);
-        list.insertAtHead("ferdia",100,"data science",2);
-        list.insertAtHead("noah",12,"tpm",1);
+        System.out.println("b".compareTo("a"));
+        list.insertAlphabetically("Bunny", 26, "CSSE", 2);
+        System.out.println("we got here 1");
+        list.insertAlphabetically("Larry", 18, "ARTS", 1);
+        System.out.println("its started 2");
+        list.insertAlphabetically("Steve", 32, "ComputationalThinking", 4);
+        System.out.println("its started 3");
 
-        handleInput(list,"print list");
-        //System.out.print('\n');
-        handleInput(list,"print list");
-    }
+        //populateList(list);
 
-    //  Printing List // Deleting Links // Getting links w/ specific value user enters
-    static public void handleInput(LinkedList list,String wePrinting)
-    {
-        if(wePrinting.equalsIgnoreCase("print list"))
+        String action = "";
+        String param;
+        String paramValue;
+        while(!action.equalsIgnoreCase("quit"))
         {
-            list.printList();
-        }
+            System.out.println("PRINT / REMOVE / PRINT LIST / NEW LINK /  QUIT");
+            action = sc.nextLine();
+            if(action.equalsIgnoreCase("PRINT LIST"))
+            {
+                parseInput(list,"doesnt matter","doesnt matter",action);
+                continue;   //skip rest of loop if user wants to print list
+            }
+            else if(action.equalsIgnoreCase("quit"))
+            {
+                break;  
+            }
+            else if(action.equalsIgnoreCase("new link"))
+            {
+                System.out.println("Enter attributes Name | Age | Course | Year of Study separated by spaces");
+                String input = sc.nextLine();
+                String[] attributes = input.split("\s{1,}");
+                list.insertAlphabetically(attributes[0],Integer.parseInt(attributes[1]),attributes[2] ,Integer.parseInt(attributes[3]));
+                continue;
+            }
+            System.out.println("Enter parameter to search by: Age, Name, Course, Year of study");
+            param = sc.nextLine();
+            System.out.println("Enter parameter value to search for:");
+            paramValue = sc.nextLine();
+
+            System.out.println("First node in the list with " + param + " value " + paramValue + " is:");
+            parseInput(list, param, paramValue, action);
+        } 
+        sc.close(); 
     }
 
-    static public void handleInput(LinkedList list, String paramName, String paramValue, String action)
+
+
+    //  Printing List // Deleting/Getting links w/ specific value user enters
+
+    static public void parseInput(LinkedList list, String paramName, String paramValue, String action)
     {
         if(action.equalsIgnoreCase("print"))
         {
-            //find all nodes with passed paramName = paramValue, and print them
+            list.printLink(paramName, paramValue);
         }
         else if(action.equalsIgnoreCase("remove"))
         {
-            //find all nodes with passed paramName = paramValue, delete them
+            list.removeLink(paramName, paramValue);
+        }
+        else if(action.equalsIgnoreCase("print list"))
+        {
+            list.printList();   //when we pass print list, pass paramName = "", paramValue = ""
         }
     }
+
+    //populating list with inputs
+    
+    public static void populateList(LinkedList list)
+    {
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+        int age = 0;
+        String course = "";
+        int yearOfStudy = 0;
+        String stopOrNot = "";
+
+        while(!stopOrNot.equalsIgnoreCase("N"))
+        {
+            //taking input
+            System.out.println("Enter name");
+            name = sc.nextLine();
+            System.out.println("Enter age");
+            age = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter course");
+            course = sc.nextLine();
+            System.out.println("Enter year of study");
+            yearOfStudy = Integer.parseInt(sc.nextLine());
+
+            //adding to linked list + stopping 
+            list.insertAlphabetically(name, age, course, yearOfStudy);
+            System.out.println("Continue populating list? Y/N");
+            stopOrNot = sc.nextLine();
+        }
+    } 
 }
--- CS210LabSeven/LinkedList.java
+++ CS210LabSeven/LinkedList.java
@@ -9,7 +9,7 @@ public class LinkedList
             first = null;
         }
 
-        ////      MAIN METHODS      ////
+        ////      GENERIC INSERTION/REMOVAL      ////
         
         public void insertAtHead(String name, int age, String course, int yearOfStudy)
         {
@@ -24,13 +24,169 @@ public class LinkedList
             }
         }
 
-        public void removeAtHead()
+        public void insertAlphabetically(String name, int age, String course, int yearOfStudy)
         {
-            first = first.next;
+            Link newLink = new Link(name,age,course,yearOfStudy);
+            if(first == null)
+            {
+                first = newLink;
+            }
+            Link current = first;
+            Link previous = null;
+            while(current != null)
+            {
+                if(current.name.compareTo(newLink.name) < 0)
+                {
+                    if(current == first)
+                    {
+                        first.previous = newLink;
+                        newLink.next = first;
+                        first = newLink;
+                    }
+                    else
+                    {
+                        previous.next = newLink;
+                        newLink.previous = previous;
+                        newLink.next = current;
+                        current.previous = newLink;
+                    }
+                }
+                previous = current;
+                current = current.next;
+            }
         }
         
-        //TODO: add deleteLink(String paramName, paramValue)
-        //TODO: add printLink(String paramName, paramValue) 
+        public void removeLink(Link current)      //remove a specific link
+        {
+            if(current == first)    //element to be removed is the first in list
+            {
+                first = first.next;
+                first.previous = null;
+            }
+            else if(current.next != null)   //element to be removed is somewhere in the middle
+            {
+            current.previous.next = current.next;
+            current.next.previous = current.previous;
+            }
+            else    //element to be removed is last
+            {
+                current.previous.next = null;
+            }
+        }
+        
+        ////        REMOVAL/PRINTING BY VALUE       ////
+
+        public void removeLink(String paramName, String paramValue)
+        {
+            Link linkToBeRemoved = null;
+            if(paramName.equalsIgnoreCase("name"))               //NAME
+            {
+                linkToBeRemoved = this.getLinkByValue(1,paramValue);
+            }
+            else if(paramName.equalsIgnoreCase(("age")))                       //AGE
+            {
+                linkToBeRemoved = this.getLinkByValue(2,paramValue);
+            }
+            else if(paramName.equalsIgnoreCase("course"))       //COURSE
+            {
+                linkToBeRemoved = this.getLinkByValue(3,paramValue);
+            }
+            else if(paramName.equalsIgnoreCase("year of study"))//YEAR OF STUDY   
+            {
+                linkToBeRemoved = this.getLinkByValue(4,paramValue);
+            }
+            removeLink(linkToBeRemoved);
+        }
+    
+        public void printLink(String paramName, String paramValue)
+        {
+            Link linkToBePrinted = null;
+            if(paramName.equalsIgnoreCase("name"))               //NAME
+            {
+                linkToBePrinted = this.getLinkByValue(1,paramValue);
+            }
+            else if(paramName.equalsIgnoreCase(("age")))                       //AGE
+            {
+                linkToBePrinted = this.getLinkByValue(2,paramValue);
+
+            }
+            else if(paramName.equalsIgnoreCase("course"))       //COURSE
+            {
+                linkToBePrinted = this.getLinkByValue(3,paramValue);
+            }
+            else if(paramName.equalsIgnoreCase("year of study"))//YEAR OF STUDY   
+            {
+                linkToBePrinted = this.getLinkByValue(4,paramValue);
+            }
+
+            if(linkToBePrinted != null)
+            {
+                printLink(linkToBePrinted);
+            }
+            else
+            {
+                System.out.println("Link with " + paramName + " value of: " + paramValue + " not found :(");
+            }
+        }
+
+        ////        FIND LINK WITH MATCHING VALUE       ////  
+
+        // Name = 1 ||  Age = 2  ||  Course = 3 ||  Year of study = 4
+        public Link getLinkByValue(int paramNumber, String paramValue)
+        {
+            Link current = first;
+            Link foundLink = null;
+            int paramInt;
+            if(paramNumber == 2 || paramNumber == 4)    //ensuring a valid number is passed when checkin age/yos
+            {
+                try{
+                    paramInt = Integer.parseInt(paramValue);
+                }
+                catch(NumberFormatException e){
+                    System.out.println(paramValue + " could not be converted to a number");
+                    return null;
+                }
+            }
+            while(current != null)
+            {
+                if(paramNumber == 1)        //name
+                {
+                    if(current.name.equalsIgnoreCase(paramValue))
+                    {
+                        foundLink = current;
+                        break;
+                    }
+                }
+                else if(paramNumber == 2)   //age
+                {
+                    paramInt = Integer.parseInt(paramValue);
+                    if(current.age == paramInt)
+                    {
+                        foundLink = current;
+                        break;
+                    }
+                }
+                else if(paramNumber == 3)   //course
+                {
+                    if(current.course.equalsIgnoreCase(paramValue))
+                    {
+                        foundLink = current;
+                        break;
+                    }
+                }
+                else if(paramNumber == 4)   //year of study
+                {
+                    paramInt = Integer.parseInt(paramValue);
+                    if(current.age == paramInt)
+                    {
+                        foundLink = current;
+                        break;
+                    }
+                }
+                current = current.next;
+            }
+            return foundLink;
+        }
 
         ////      MISC METHODS        ////  
 
@@ -39,33 +195,31 @@ public class LinkedList
             return (first == null);
         }
 
-        public Link getFirst()
-        {
-            return first;
-        }
-
-
         public void printList()
         {
             Link current = first;
-            while(current != null)
+            if(!this.isEmpty())
             {
-                printLink(current);
-                if(current.next != null)
+                while(current != null)
                 {
-                    current = current.next;
-                }
-                else{
-                    break;
+                    printLink(current);
+                    if(current.next != null)
+                    {
+                        current = current.next;
+                    }
+                    else{
+                        break;
+                    }
                 }
             }
+            else
+            {
+                System.out.println("The linked list is empty");
+            }
         }
-
+        
         public void printLink(Link link)
         {
-            System.out.print( '\n' + link.name + ", ");
-            System.out.print(link.age + ", ");
-            System.out.print(link.course + ", ");
-            System.out.print(link.yearOfStudy + '\n');
+            System.out.println(link.name + "," + link.age + "," + link.course + "," + link.yearOfStudy);
         }
 }
